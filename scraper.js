8b3b02d14f973979dc597f6fa3930010e953d579
user <user@lab1560.tumo.lab>
2017-09-26 19:27:09 +0400

    final of scr

--- scraper.js
+++ scraper.js
@@ -1,41 +1,53 @@
 const scrapeIt = require("scrape-it");
 var jsonfile = require('jsonfile')
 var file = 'data.json'
-var i = 1, arr =[] , site_link;
+var i = 1, arr = {"cars":[]}, site_link;
 
-var interval= setInterval(function(){
-    console.log(site_link);
+var interval = setInterval(function () {
     i++;
     site_link = "https://www.list.am/category/23/" + i;
-    scrapeIt( site_link , {
-    cars: {
-        listItem: ".h",
-        data: {
-            image: {
-                selector: 'img',
-                attr: "src"
-            },
-            title: "div.l",
-            price: ".l2 .l",
-            url: {
-                closest: "a",
-                attr: "href"
+    scrapeIt(site_link, {
+        cars: {
+            listItem: ".h",
+            data: {
+                image: {
+                    selector: 'img',
+                    attr: "src"
+                },
+                title: "div.l",
+                year: {
+                    selector: "div.l",
+                    convert: function(x){
+                        var y = x.split(' ');
+                        for(i=0;i<y.length;i++){
+                            if( Number(y[1]) >=1800 && Number(y[1])<=2017){
+                                console.log("iaudoiausdoiuaoduaoiduoiasudoiuasoduaosudoaisudopiasudoiuasopiduaopsiudpoiasudpoiausopdiuapoisduopiasudpoiuasopdiuaposiduopaisudpoiasupodiuaopsidu" + Number(y[i]));
+                            }
+                        }
+                        //x = x.split('թ')[0]
+                        return x;
+                    }
+                },
+                price: ".l2 .l",
+                url: {
+                    closest: "a",
+                    attr: "href"
+                }
             }
+
         }
 
+    }, (err, page) => {
+        arr.cars.push(...page.cars)
+    })
+
+    if (i == 5) {
+        console.log(arr);
+        jsonfile.writeFile(file, arr, { spaces: 2 }, function (err) {
+            console.error(err)
+        })
+        clearInterval(interval);
     }
 
-}, (err, page) => {
-        arr.push(page)
-    })
-    
-    if(i==50){
-            console.log(arr);
-            jsonfile.writeFile(file, arr, { spaces: 2 }, function (err) {
-                console.error(err)
-            })          
-            clearInterval(interval);
-        }
 
-
-},10);
+}, 1000);
